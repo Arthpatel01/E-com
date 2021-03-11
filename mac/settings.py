8b827a18b9c0594289ecf9482c6dc29b4aab6084
Arthpatel01 <arthpatel.ce@gmail.com>
2021-03-11 20:11:49 +0530

    change

--- mac/settings.py
+++ mac/settings.py
@@ -79,22 +79,23 @@ WSGI_APPLICATION = 'mac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'arth',
-        'PASSWORD': '1828',
-        'HOST': 'abpatel.herokuapp.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'mydatabase',
+#        'USER': 'arth',
+#        'PASSWORD': '1828',
+#        'HOST': 'abpatel.herokuapp.com',
+#        'PORT': '5432',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
